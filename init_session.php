33a88003cbd693e9d11ee11597d5782358209c42
Andrei Smoliakov <main@avsm.me>
2025-09-07 17:37:30 +0300

    Update frontend to support new DB format

--- init_session.php
+++ init_session.php
@@ -1,38 +1,44 @@
 <?php
 
+require "frontend_data_generation.php";
+
 session_start();
 
-$users = include '../users.php';
+$USER_DB = load_json("../user_db.json");
+$SERVER_DB = load_json("../server_db.json");
+
 $logged_in = false;
 
 if(isset($_REQUEST['login'])){
-  $username = $_REQUEST['username'];
-  $password = $_REQUEST['password'];
+	$username = $_REQUEST['username'];
+	$password = $_REQUEST['password'];
+	$pwd_hash = find_user($username, $USER_DB)->password_hash ?? null;
 
-  if( isset($users[$username]) && password_verify($password, $users[$username]['password_hash']) ) {
-    $_SESSION['username'] = $username;
-    $_SESSION['LOGIN_TIME'] = time();
-  }
+	if( password_verify($password, $pwd_hash) ) {
+		$_SESSION['username'] = $username;
+		$_SESSION['LOGIN_TIME'] = time();
+	}
 }
 
 if(isset($_REQUEST['logout'])){
-  session_unset();
-  session_destroy();
-  header("Location: /");
-  exit();
+	session_unset();
+	session_destroy();
+	header("Location: /");
+	exit();
 }
 
 if (isset($_SESSION['LOGIN_TIME']) && (time() - $_SESSION['LOGIN_TIME'] > 600)) {
-  session_unset();
-  session_destroy();
-  session_start();
+	session_unset();
+	session_destroy();
+	session_start();
 }  
 
 if (!empty($_SESSION['username'])) {
-  $logged_in = true;
-  $username = $_SESSION['username'];
-  $user_data = $users[$username];
-  $srvs = include '../servers_data.php';
+	$logged_in = true;
+	$username = $_SESSION['username'];
+
+	$FRONTEND_DATA = generate_frontend_data(
+		find_user($username, $USER_DB), $SERVER_DB, 'dummy3');
 }
 
 ?>
